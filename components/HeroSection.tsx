77968bcda5ecdcf7b50949400b23c710a203d00d
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-03-27 05:11:36 +0100

    Refactor HeroSection component for improved layout and readability

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,20 +3,20 @@ import React from "react";
 
 const HeroSection = () => {
   return (
-    <div className="bg-[url('/assets/images/background.png')] relative bg-cover bg-center md:px-8 lg:px-60 w-full h-dvh flex flex-col items-center justify-center">
-      <div className="text-center text-[#FAFAFA] flex flex-col items-center gap-4 md:gap-6">
-        <h2 className="text-3xl md:text-4xl lg:text-[70px] leading-10 md:leading-12 lg:leading-20 capitalize font-medium ">
+    <div className="h-dvh relative flex w-full flex-col items-center justify-center bg-[url('/assets/images/background.png')] bg-cover bg-center md:px-8 xl:px-60">
+      <div className="flex flex-col items-center gap-4 text-center text-[#FAFAFA] md:gap-6">
+        <h2 className="md:leading-12 lg:leading-20 text-3xl font-medium capitalize leading-10 md:text-4xl lg:text-[70px]">
           Find the perfect furniture to complete your home
         </h2>
-        <p className="text-base md:text-xl font-normal max-w-[340px] md:max-w-[600px] lg:max-w-[650px] ">
+        <p className="max-w-[340px] text-base font-normal md:max-w-[600px] md:text-xl lg:max-w-[650px]">
           We specialize in buying and selling high-quality, marketable
           furniture, each piece reflecting our unique aesthetic.
         </p>
-        <button className="bg-[#FAFAFA] h-14 w-44 p-2.5 rounded-[50px] text-slate-950 text-xl font-medium mt-8 lg:mt-5">
+        <button className="mt-8 h-14 w-44 rounded-[50px] bg-[#FAFAFA] p-2.5 text-xl font-medium text-slate-950 lg:mt-5">
           Shop Now
         </button>
       </div>
-      <span className=" absolute bottom-8 flex items-center justify-center mt-10 border border-white rounded-full w-12 h-12 bg-transparent hover:bg-white/10  transition duration-300 ease-in-out animate-bounce motion-reduce:animate-none">
+      <span className="absolute bottom-8 mt-10 flex h-12 w-12 animate-bounce items-center justify-center rounded-full border border-white bg-transparent transition duration-300 ease-in-out hover:bg-white/10 motion-reduce:animate-none">
         <Image
           src={"/assets/icons/Arrow Down.svg"}
           height={50}
