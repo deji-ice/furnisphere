96cbc13e7496ac3931ce27f0984a24acf60d8b60
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-03-27 05:18:27 +0100

    Refactor Offers component for improved layout and readability

--- components/Offers.tsx
+++ components/Offers.tsx
@@ -25,27 +25,29 @@ const Offers = () => {
     },
   ];
   return (
-    <section className="my-14 px-3 space-y-6">
+    <section className="my-14 space-y-6 px-3">
       <div className="flex flex-col items-center justify-center gap-2.5 lg:gap-5">
-        <h2 className="text-3xl lg:text-5xl text-center font-medium">
+        <h2 className="text-center text-3xl font-medium lg:text-5xl">
           What We Can Offer You
         </h2>
-        <p className="text-[15px] w-[395px] md:w-full px-2 text-center text-[#878787] lg:text-xl font-normal   lg:max-w-[650px] ">
+        <p className="w-[395px] px-2 text-center text-[15px] font-normal text-[#878787] md:w-full lg:max-w-[680px] lg:text-xl">
           High-quality, stylish, and functional furniture designed to elevate
           your space with comfort and elegance.
         </p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  md:max-w[800px] max-w-[1300px] place-items-center mx-auto gap-5">
+      <div className="md:max-w[800px] mx-auto grid max-w-[1300px] grid-cols-1 place-items-center gap-5 md:grid-cols-2 lg:grid-cols-3">
         {offers.map((offer, index) => (
           <div
             key={index}
-            className="bg-[#F2F2F2] md:last:col-span-2 lg:last:col-span-1 border-[1.25px] border-[#EAEAEA] rounded-3xl flex flex-col justify-center items-center h-[335px] w-[368px] md:w-[348px]"
+            className="flex h-[335px] w-[368px] flex-col items-center justify-center rounded-3xl border-[1.25px] border-[#EAEAEA] bg-[#F2F2F2] text-center md:w-[348px] md:last:col-span-2 lg:w-[300px] lg:last:col-span-1 xl:w-[420px] xl:w-[]"
           >
             <Image height={72} width={72} src={offer.icon} alt="" />
-            <h3 className="text-[40px] font-normal mt-5 text-[#1E1E1E]">
+            <h3 className="mt-5 text-[40px] font-normal text-[#1E1E1E] lg:text-4xl">
               {offer.title}
             </h3>
-            <p className="text-[#5E5E5E] text-lg">{offer.description}</p>
+            <p className="text-lg text-[#5E5E5E] lg:mt-3">
+              {offer.description}
+            </p>
           </div>
         ))}
       </div>
