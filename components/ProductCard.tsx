d088cd084799beb56430e4b335e8c6cfa9fd4bed
Ayodeji Atanda <ayodejiatanda1@gmail.com>
2025-03-28 19:22:38 +0100

    refactor ProductCard component for improved structure and performance
    - Adjusted the order of class names for better readability.
    - Simplified the structure of the badge and loading indicators.
    - Added lazy loading to images for enhanced performance.

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -5,14 +5,14 @@ const ProductCard = () => {
   return (
     <div className="flex h-[375px] w-[365px] flex-col items-center justify-between rounded-3xl bg-[#F2F2F2] p-5 lg:h-[340px] lg:w-[300px] xl:h-[420px] xl:w-[410px]">
       <div className="flex w-full items-center justify-between">
-        <span className="rounded-4xl flex h-[47.3px] w-[113.7px] items-center justify-center bg-white text-lg font-medium">
+        <span className="flex h-[47.3px] w-[113.7px] items-center justify-center rounded-4xl bg-white text-lg font-medium">
           Best Sale
         </span>
         <div className="relative h-[30px] w-fit bg-black">
-          <div className="h-5.5 w-5.5 absolute right-[50px] z-10 rounded-full bg-[#1E1E1E]" />
-          <div className="h-5.5 w-5.5 absolute right-[34px] z-20 rounded-full bg-[#585453]" />
-          <div className="h-5.5 w-5.5 absolute right-[16px] z-30 rounded-full bg-[#CE9606]" />
-          <div className="h-5.5 w-5.5 absolute right-0 z-40 rounded-full bg-[#c8c0bd]" />
+          <div className="absolute right-[50px] z-10 h-5.5 w-5.5 rounded-full bg-[#1E1E1E]" />
+          <div className="absolute right-[34px] z-20 h-5.5 w-5.5 rounded-full bg-[#585453]" />
+          <div className="absolute right-[16px] z-30 h-5.5 w-5.5 rounded-full bg-[#CE9606]" />
+          <div className="absolute right-0 z-40 h-5.5 w-5.5 rounded-full bg-[#c8c0bd]" />
         </div>
       </div>
       <Image
@@ -21,6 +21,7 @@ const ProductCard = () => {
         height={300}
         width={300}
         className="lg:w-[420px]"
+        loading="lazy"
       />
       <div className="flex w-full items-center justify-between">
         <div className="flex flex-col items-start">
@@ -32,6 +33,7 @@ const ProductCard = () => {
           height={50}
           width={50}
           alt="shopping bag icon"
+          loading="lazy"
           className={`h-12 w-12 rounded-xl bg-white p-3`}
         />
       </div>
